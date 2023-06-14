83d2183e2b32b77063323491f3738ef45eefdb78
shehabkhalel <shehab.ahmed@reno.systems>
2023-06-14 12:38:51 +0300

    add translation

--- src/UI-Components/Layout/Footer/Footer.js
+++ src/UI-Components/Layout/Footer/Footer.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 
 function Footer() {
+  const { t } = useTranslation();
+
   return (
     <footer>
-      <p>&copy; {new Date().getFullYear()} My React App</p>
+      <p>
+        &copy; {new Date().getFullYear()} {t("COPYRIGHT")}
+      </p>
     </footer>
   );
 }
