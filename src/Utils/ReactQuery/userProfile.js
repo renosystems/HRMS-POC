cd77eae872f3571ee13d3799482d8bd40d438c1d
shehabkhalel <shehab.ahmed@reno.systems>
2023-06-26 16:59:16 +0300

    react-query related directories

--- src/Utils/ReactQuery/userProfile.js
+++
@@ -1,8 +0,0 @@
-import { useFetch } from "./Fetcher";
-import { apiRoutes } from "../Api/Api";
-
-export const useGetProfile = () => {
-  const context = useFetch(apiRoutes.getProfile, undefined, { retry: false });
-  //We use the retry: false setting here because we don’t want to retry this request. If it fails, we believe that the user is unauthorized and do the redirect.
-  return { ...context, data: context.data?.user };
-};
