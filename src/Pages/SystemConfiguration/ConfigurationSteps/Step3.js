c751f75d7b18d7ff1e88325ab192f479e612bd5e
shehabkhalel <shehab.ahmed@reno.systems>
2023-07-06 16:08:22 +0300

    add step3 and 4

--- src/Pages/SystemConfiguration/ConfigurationSteps/Step3.js
+++ src/Pages/SystemConfiguration/ConfigurationSteps/Step3.js
@@ -1,11 +1,26 @@
-import React from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React, { useCallback } from "react";
 
-function Step3() {
-  const { status, config } = useSelector((state) => state.config);
-  const dispatch = useDispatch();
+/**
+ * @param {Object} settings account settings details
+ * @param {Object} nextStep next step handler fn
+ * @param {Object} stepBackHandler back step handler fn
+ * @returns
+ */
+function Step3({ settings, nextStep, stepBackHandler }) {
+  const handleNext = useCallback(() => {
+    const newSettings = { ...settings, approvalCycle: "onlyHiringRequest" };
+    nextStep(newSettings, "step3");
+  }, [nextStep, settings]);
 
-  return <>step3</>;
+  return (
+    <>
+      <h1>step3</h1>
+      <p>{settings.approvalCycle}</p>
+
+      <button onClick={stepBackHandler}>back</button>
+      <button onClick={handleNext}>Next</button>
+    </>
+  );
 }
 
 export default Step3;
--- src/Pages/SystemConfiguration/ConfigurationSteps/Step4.js
+++ src/Pages/SystemConfiguration/ConfigurationSteps/Step4.js
@@ -1,11 +1,26 @@
-import React from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React, { useCallback } from "react";
 
-function Step4() {
-  const { status, config } = useSelector((state) => state.config);
-  const dispatch = useDispatch();
+/**
+ * @param {Object} settings account settings details
+ * @param {Object} nextStep next step handler fn
+ * @param {Object} stepBackHandler back step handler fn
+ * @returns
+ */
+function Step4({ settings, nextStep, stepBackHandler }) {
+  const handleNext = useCallback(() => {
+    const newSettings = { ...settings, applicantForm: "detailed" };
+    nextStep(newSettings, "step4");
+  }, [nextStep, settings]);
 
-  return <>step4</>;
+  return (
+    <>
+      <h1>step4</h1>
+      <p>{settings.applicantForm}</p>
+
+      <button onClick={stepBackHandler}>back</button>
+      <button onClick={handleNext}>Next</button>
+    </>
+  );
 }
 
 export default Step4;
