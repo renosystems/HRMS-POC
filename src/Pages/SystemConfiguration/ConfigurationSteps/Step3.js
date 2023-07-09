4ff2170a392e7563c4cc4891636caf85c484e96d
shehabkhalel <shehab.ahmed@reno.systems>
2023-07-09 12:03:17 +0300

    same as prev for step3

--- src/Pages/SystemConfiguration/ConfigurationSteps/Step3.js
+++ src/Pages/SystemConfiguration/ConfigurationSteps/Step3.js
@@ -1,5 +1,5 @@
-import React, { useCallback } from "react";
-
+import React, { useCallback, useState } from "react";
+import { Pane, Text, Heading, RadioGroup, Button } from "evergreen-ui";
 /**
  * @param {Object} settings account settings details
  * @param {Object} nextStep next step handler fn
@@ -7,19 +7,100 @@ import React, { useCallback } from "react";
  * @returns
  */
 function Step3({ settings, nextStep, stepBackHandler }) {
+  const [value, setValue] = useState(settings.approvalCycle);
+  const [changed, setChanged] = useState(false);
+
+  const handleChange = useCallback(
+    (e) => {
+      if (!changed) setChanged(true);
+      setValue(e.target.value);
+    },
+    [changed]
+  );
+
   const handleNext = useCallback(() => {
-    const newSettings = { ...settings, approvalCycle: "onlyHiringRequest" };
+    const newSettings = changed ? { ...settings, approvalCycle: value } : null;
+
     nextStep(newSettings, "step3");
-  }, [nextStep, settings]);
+  }, [changed, nextStep, settings, value]);
 
   return (
-    <>
-      <h1>step3</h1>
-      <p>{settings.approvalCycle}</p>
+    <Pane
+      marginTop="50px"
+      marginX="auto"
+      paddingY="70px"
+      paddingX="50px"
+      width="60%"
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+      border="default"
+    >
+      <Heading>Let's get started configuring your system</Heading>
+      <Text paddingY="10px" size={300} color="muted">
+        We'll help you get setup based on your business needs
+      </Text>
 
-      <button onClick={stepBackHandler}>back</button>
-      <button onClick={handleNext}>Next</button>
-    </>
+      <hr style={{ width: "100%", color: "#E6E8F0", margin: "20px 0px" }} />
+
+      <Heading>
+        Would you like to implement approval cycle for job post, hiring request
+        or both
+      </Heading>
+      <Text
+        paddingY="10px"
+        textAlign="center"
+        width="60%"
+        size={300}
+        color="muted"
+      >
+        Making approval cycle for the hiring request, job post or both pf them
+      </Text>
+
+      <Pane display="flex" justifyContent="center" width="50%">
+        <RadioGroup
+          value={value}
+          options={[
+            {
+              label: "Only Hiring request approval cycle",
+              value: "hiringRequest",
+            },
+            { label: "Only job post approval cycle", value: "jobPost" },
+            { label: "Approval cycle for both", value: "both" },
+          ]}
+          onChange={handleChange}
+          display="flex"
+          flexDirection="column"
+          width="50%"
+        />
+      </Pane>
+
+      <Pane
+        display="flex"
+        justifyContent="space-between"
+        alignItems="flex-end"
+        width="100%"
+        height="100px"
+      >
+        <Button
+          onClick={stepBackHandler}
+          appearance="default"
+          paddingY="20px"
+          paddingX="35px"
+        >
+          Back
+        </Button>
+        <Button
+          onClick={handleNext}
+          appearance="main"
+          paddingY="20px"
+          paddingX="35px"
+        >
+          Next
+        </Button>
+      </Pane>
+    </Pane>
   );
 }
 
