c9719b59651f9f3cd0cd76efd7c17e4137cc52ab
shehabkhalel <shehab.ahmed@reno.systems>
2023-07-10 16:05:44 +0300

    add step7

--- src/Pages/SystemConfiguration/ConfigurationSteps/Step7.js
+++ src/Pages/SystemConfiguration/ConfigurationSteps/Step7.js
@@ -5,70 +5,48 @@ import {
   Heading,
   Button,
   Spinner,
-  FileCard,
-  FileUploader,
+  NewGridItemIcon,
+  Radio,
   TextInput,
 } from "evergreen-ui";
 
 import { useDispatch, useSelector } from "react-redux";
 import {
-  addEmployee,
-  getEmployees,
-} from "../../../Utils/RTK/slices/employees.slice";
+  addDepartment,
+  getDepartments,
+} from "../../../Utils/RTK/slices/departments.slice";
 import { updateConfiguration } from "../../../Utils/RTK/slices/config.slice";
 
 /**
- * @param {Object} nextStep next step handler fn
  * @param {Object} stepBackHandler back step handler fn
  * @returns
  */
-function Step6({ nextStep, stepBackHandler }) {
-  const { status, employees } = useSelector((state) => state.employees);
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [phoneNumber, setPhoneNumber] = useState("");
-  const [title, setTitle] = useState("");
+function Step7({ stepBackHandler }) {
+  const { status, departments } = useSelector((state) => state.departments);
+  const [isAddFirstClicked, setIsAddFirstClicked] = useState(false);
+  const [isAddingDepartment, setIsAddingDepartment] = useState(false);
+  const [currentStep, setCurrentStep] = useState(1);
 
-  const [isAddingManager, setIsAddingManager] = useState(false);
-  const [isAddingExcutive, setIsAddingExcutive] = useState(false);
-
-  const [files, setFiles] = useState([]);
-  const [fileRejections, setFileRejections] = useState([]);
-  const handleChange = useCallback((files) => setFiles([files[0]]), []);
-  const handleRejected = useCallback(
-    (fileRejections) => setFileRejections([fileRejections[0]]),
-    []
-  );
-  const handleRemove = useCallback(() => {
-    setFiles([]);
-    setFileRejections([]);
-  }, []);
+  const [order, setOrder] = useState("ascending");
+  const [depName, setDepName] = useState("");
 
   const dispatch = useDispatch();
 
+  const handleFinish = useCallback(() => {
+    dispatch(updateConfiguration("step7"));
+  }, [dispatch]);
+
   const handleNext = useCallback(() => {
-    dispatch(updateConfiguration("step6"));
-
-    nextStep();
-  }, [dispatch, nextStep]);
-
-  const handleAdd = useCallback(() => {
-    dispatch(
-      addEmployee({
-        name: name,
-        email: email,
-        phoneNumber: phoneNumber,
-        title: title,
-        role: isAddingManager ? "manager" : "excutive",
-      })
-    );
-
-    if (isAddingManager) setIsAddingManager(false);
-    else setIsAddingExcutive(false);
-  }, [dispatch, email, isAddingManager, name, phoneNumber, title]);
+    if (currentStep === 1) {
+      setCurrentStep(2);
+    } else {
+      dispatch(addDepartment({ order: order, name: depName }));
+      setIsAddingDepartment(false);
+    }
+  }, [currentStep, depName, dispatch, order]);
 
   useEffect(() => {
-    if (status === "idle") dispatch(getEmployees());
+    if (status === "idle") dispatch(getDepartments());
   }, [dispatch, status]);
 
   if (status === "idle" || status === "loading") {
@@ -92,76 +70,50 @@ function Step6({ nextStep, stepBackHandler }) {
       justifyContent="center"
       border="default"
     >
-      {!isAddingExcutive && !isAddingManager ? (
+      {!isAddFirstClicked && !departments.length ? (
         <Pane
           minHeight="60vh"
           marginX="auto"
-          paddingY="50px"
+          paddingY="90px"
           paddingX="50px"
           width="40%"
           display="flex"
           flexDirection="column"
           justifyContent="space-between"
+          alignItems="center"
           background="gray300"
           border="default"
         >
-          <Pane>
-            <Heading>
-              Add managers and excutives to your system, and upon it you will
-              assign managers to departments
-            </Heading>
+          <Pane textAlign="center" marginX="auto">
+            <NewGridItemIcon size={30} color="muted" marginBottom="10px" />
+            <Heading>Add your first department</Heading>
             <Text paddingY="10px" size={300} color="muted">
-              Please note that excutives are higher in the heirarchy
+              Add your departments to the system to users of each department
             </Text>
             <Button
               onClick={() => {
-                setIsAddingManager(true);
+                setIsAddFirstClicked(true);
+                setIsAddingDepartment(true);
               }}
-              background="white"
+              appearance="main"
               color="grey"
               width="100%"
               marginBottom="15px"
               marginTop="20px"
             >
-              Add managers
-            </Button>
-            <Button
-              onClick={() => {
-                setIsAddingExcutive(true);
-              }}
-              background="white"
-              color="grey"
-              width="100%"
-            >
-              Add excutives
+              Add Department
             </Button>
           </Pane>
 
-          <Pane
-            display="flex"
-            justifyContent={employees.length ? "space-between" : "flex-start"}
-            alignItems="flex-end"
-            width="100%"
-            padding="auto"
-          >
+          <Pane>
             <Button
               onClick={stepBackHandler}
               appearance="default"
               paddingY="20px"
               paddingX="35px"
             >
-              Back
+              Back to Managers
             </Button>
-            {employees.length && (
-              <Button
-                onClick={handleNext}
-                appearance="main"
-                paddingY="20px"
-                paddingX="35px"
-              >
-                Skip to departments
-              </Button>
-            )}
           </Pane>
         </Pane>
       ) : (
@@ -175,96 +127,148 @@ function Step6({ nextStep, stepBackHandler }) {
           background="gray300"
           border="default"
         >
-          <Heading>Adding {isAddingManager ? "manager" : "excutive"}</Heading>
+          <Heading>
+            Create departments and assign managers that previously created to
+            your department
+          </Heading>
           <Text paddingY="10px" size={300} color="muted">
-            Add your {isAddingManager ? "manager" : "excutive"} details
+            Create departments and assign managers and excutives
           </Text>
 
-          <FileUploader
-            label="Change profile image"
-            maxSizeInBytes={50 * 1024 ** 2}
-            maxFiles={1}
-            onChange={handleChange}
-            onRejected={handleRejected}
-            renderFile={(file) => {
-              const { name, size, type } = file;
-              const fileRejection = fileRejections.find(
-                (fileRejection) => fileRejection.file === file
-              );
-              const { message } = fileRejection || {};
-              return (
-                <FileCard
-                  key={name}
-                  isInvalid={fileRejection != null}
-                  name={name}
-                  onRemove={handleRemove}
-                  sizeInBytes={size}
-                  type={type}
-                  validationMessage={message}
-                />
-              );
-            }}
-            values={files}
-          />
-
-          <TextInput
-            onChange={(e) => setName(e.target.value)}
-            value={name}
-            placeholder="Name"
-            width="100%"
-            marginBottom="20px"
-          />
-
-          <TextInput
-            onChange={(e) => setEmail(e.target.value)}
-            value={email}
-            placeholder="email"
-            type="email"
-            width="100%"
-            marginBottom="20px"
-          />
-          <TextInput
-            onChange={(e) => setPhoneNumber(e.target.value)}
-            value={phoneNumber}
-            placeholder="Phone Number"
-            width="100%"
-            marginBottom="20px"
-          />
-          <TextInput
-            label="title"
-            onChange={(e) => setTitle(e.target.value)}
-            value={title}
-            placeholder="Title"
-            width="100%"
-            marginBottom="20px"
-          />
-
-          <Pane display="flex" justifyContent="space-between" width="100%">
-            <Button
-              onClick={() => {
-                isAddingManager
-                  ? setIsAddingManager(false)
-                  : setIsAddingExcutive(false);
-              }}
-              appearance="default"
-              paddingY="20px"
-              paddingX="35px"
+          {isAddingDepartment ? (
+            <Pane
+              display="flex"
+              flexDirection="column"
+              justifyContent="space-between"
+              minHeight="50vh"
             >
-              Cancel
-            </Button>
-            <Button
-              onClick={handleAdd}
-              appearance="main"
-              paddingY="20px"
-              paddingX="35px"
-            >
-              Next
-            </Button>
-          </Pane>
+              {currentStep === 1 ? (
+                <Pane aria-label="Radio Group Heirarchry levels" role="group">
+                  <Radio
+                    name="order"
+                    label="Ascending Heirarchry levels"
+                    value="ascending"
+                    onChange={(e, checked) => checked && setOrder("ascending")}
+                    fontWeight="900"
+                  />
+                  <Radio
+                    name="order"
+                    label="Descending Heirarchry levels"
+                    value="descending"
+                    onChange={(e, checked) => checked && setOrder("ascending")}
+                  />
+                </Pane>
+              ) : (
+                <Pane>
+                  <TextInput
+                    onChange={(e) => setDepName(e.target.value)}
+                    value={depName}
+                    placeholder="Department name"
+                    width="100%"
+                    marginBottom="20px"
+                  />
+                </Pane>
+              )}
+
+              <Pane
+                display="flex"
+                justifyContent="space-between"
+                alignItems="flex-end"
+                width="100%"
+                padding="auto"
+                paddingY="20px"
+              >
+                <Button
+                  onClick={() => {
+                    if (currentStep === 2) setCurrentStep(1);
+                    else {
+                      setIsAddFirstClicked(false);
+                      setIsAddingDepartment(false);
+                    }
+                  }}
+                  appearance="default"
+                  paddingY="20px"
+                  paddingX="35px"
+                >
+                  Back
+                </Button>
+
+                <Button
+                  onClick={handleNext}
+                  appearance="main"
+                  paddingY="20px"
+                  paddingX="35px"
+                >
+                  Next
+                </Button>
+              </Pane>
+            </Pane>
+          ) : (
+            <>
+              <Pane>
+                {departments.map((dep) => (
+                  <Button
+                    background="white"
+                    color="grey"
+                    width="100%"
+                    marginY="10px"
+                    key={dep.name}
+                  >
+                    {dep.name}
+                  </Button>
+                ))}
+                <hr style={{ width: "100%" }} />
+                <Button
+                  onClick={() => {
+                    setIsAddingDepartment(true);
+                  }}
+                  background="white"
+                  color="grey"
+                  width="100%"
+                  marginY="20px"
+                >
+                  Add department
+                </Button>
+              </Pane>
+
+              <Pane
+                display="flex"
+                justifyContent="space-between"
+                alignItems="flex-end"
+                width="100%"
+                padding="auto"
+                paddingY="20px"
+              >
+                <Button
+                  onClick={() => {
+                    if (currentStep === 2) setCurrentStep(1);
+                    else {
+                      setIsAddFirstClicked(false);
+                      setIsAddingDepartment(false);
+                    }
+                  }}
+                  appearance="default"
+                  paddingY="20px"
+                  paddingX="35px"
+                >
+                  Back
+                </Button>
+
+                <Button
+                  onClick={handleFinish}
+                  appearance="main"
+                  paddingY="20px"
+                  paddingX="35px"
+                >
+                  Finish and continue
+                </Button>
+              </Pane>
+            </>
+          )}
         </Pane>
       )}
     </Pane>
   );
 }
 
-export default Step6;
+export default Step7;
