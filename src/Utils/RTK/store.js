dc813c13e75965e1be37c5fca93438c2eba97f97
shehabkhalel <shehab.ahmed@reno.systems>
2023-07-10 14:31:05 +0300

    add department slice to the store

--- src/Utils/RTK/store.js
+++ src/Utils/RTK/store.js
@@ -4,12 +4,14 @@ import authSlice from "./slices/auth.slice";
 import configSlice from "./slices/config.slice";
 import accountSettingsSlice from "./slices/accountSettings.slice";
 import employeesSlice from "./slices/employees.slice";
+import departmentsSlice from "./slices/departments.slice";
 
 export const store = configureStore({
   reducer: {
     auth: authSlice,
     config: configSlice,
     accountSettings: accountSettingsSlice,
+    departments: departmentsSlice,
     employees: employeesSlice,
 
     [departmentApiSlice.reducerPath]: departmentApiSlice.reducer,
